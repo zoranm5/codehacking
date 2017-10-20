072f474054dc9adff184c97a1bcb5cb0c910bde5
unknown <zoran.musulin@gmail.com>
2017-10-20 05:27:24 +0200

    admin master page setup

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,37 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.sass('app.scss')
+
+
+        .styles([
+
+        'libs/blog-post.css',
+        'libs/bootstrap.css',
+        'libs/font-awesome.css',
+        'libs/metisMenu.css',
+        'libs/sb-admin-2.css'
+
+
+       ], './public/css/libs.css')
+
+
+        .scripts([
+
+                'libs/bootstrap.js',
+                'libs/jquery.js',
+                'libs/metisMenu.js',
+                'libs/sb-admin-2.js',
+                'scripts.js'
+
+
+
+
+
+        ],'./public/js/libs.js')
+
+
+
+
+
 });
